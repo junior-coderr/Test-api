426dae0da680551a69b6f22e7f1ac62c3456b156
Pratik Mishra <pratikmishra1833@gmail.com>
2023-09-19 23:14:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
+
+const data = {
+    name: 'Pratik Mishra',
+    age:17,
+    gender: 'male',
+}
+
+
+app.get('/', (req, res) => {
+    res.send(data);
+
+});
+
+app.listen(port);
